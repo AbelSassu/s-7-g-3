df09de727841c39b16d70d8938b0633badba414c
Abel Sassu <relinquished@hotmail.it>
2023-12-10 23:08:02 +0100

    asd

--- script.js
+++ script.js
@@ -5,37 +5,42 @@ let shoppingCartList = JSON.parse(localStorage.getItem("shoppingCart")) || [];
 
 window.onload = () => {
     loadBooks();
-    loadShoppingCart()
+    loadShoppingCart();
 };
 
 const loadBooks = () => {
     fetch("https://striveschool-api.herokuapp.com/books")
-        .then(resp => resp.json())
-        .then(books => {
+        .then((resp) => resp.json())
+        .then((books) => {
             displayBooks(books);
             outerBooks = books;
         })
-        .catch(err =>
-            console.error(err.message)
-        )
-
-}
+        .catch((err) => console.error(err.message));
+};
 
 function displayBooks(books) {
     booksWrapper.innerHTML = "";
     books.forEach((book) => {
-        const isBookInCart = shoppingCartList.findIndex(cartBook => cartBook.title === book.title) !== -1
+        const isBookInCart =
+            shoppingCartList.findIndex(
+                (cartBook) => cartBook.title === book.title
+            ) !== -1;
         booksWrapper.innerHTML += `
     <div class="col">
-      <div class="card shadow-sm h-100 ${isBookInCart ? 'selected' : ''}">
-        <img src="${book.img}" class="img-fluid card-img-top" alt="${book.title
-            }">
+      <div class="card shadow-sm h-100 ${isBookInCart ? "selected" : ""}">
+        <img src="${book.img}" class="img-fluid card-img-top" alt="${
+            book.title
+        }">
         <div class="card-body">
           <h5 class="card-title">${book.title}</h5>
-          <p class="card-text badge rounded-pill bg-dark mb-2">${book.category}</p>
+          <p class="card-text badge rounded-pill bg-dark mb-2">${
+              book.category
+          }</p>
           <p class="fs-4">${book.price}€</p>
           <div>
-              <button class="btn btn-danger" onclick="addToCart(event, '${book.asin}')">Compra ora</button>
+              <button class="btn btn-danger" onclick="addToCart(event, '${
+                  book.asin
+              }')">Compra ora</button>
               <button class="btn btn-outline-danger" onclick="skipMe(event)">
                 Scarta
               </button>
@@ -48,19 +53,19 @@ function displayBooks(books) {
 }
 const skipMe = (event) => {
     event.target.closest('.col').remove()
-}
+};
 
 const addToCart = (event, asin) => {
     console.log(asin);
     const book = outerBooks.find((book) => book.asin === asin);
     shoppingCartList.push(book);
     console.log(shoppingCartList);
-    localStorage.setItem("shoppingCart", JSON.stringify(shoppingCartList))
+    localStorage.setItem("shoppingCart", JSON.stringify(shoppingCartList));
 
     loadShoppingCart();
 
     event.target.closest(".card").classList.add("selected");
-}
+};
 
 const loadShoppingCart = () => {
     shoppingCartList.forEach((book) => {
@@ -87,4 +92,4 @@ const loadShoppingCart = () => {
     </div>
   `;
     });
-}
+};
